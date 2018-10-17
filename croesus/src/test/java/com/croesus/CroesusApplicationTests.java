9755cdfdae4b853d39df0d391bd8162639f3b5d7
Tokiya <Tokiya@surfinX>
2018-10-18 08:23:43 +0900

    initial commit

---
+++ croesus/src/test/java/com/croesus/CroesusApplicationTests.java
@@ -0,0 +1,16 @@
+package com.croesus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CroesusApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

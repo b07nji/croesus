ecc05abf9b6ccac916c81d36fb8642078f5bd536
b07nji <b07ji.morelia@gmail.com>
2018-10-28 14:12:08 +0900

    add closeTable()

--- croesus/bin/main/static/js/fee.js
+++ croesus/bin/main/static/js/fee.js
@@ -1,118 +1,133 @@
 function compareFees() {
 	
 	var url = "/fee"
-		
 	fetch(url)
 		.then(response => {
 			return  response.json();
 			
-		}).then(json => {
-			
-			function createTable(obj, company, tableId) {
-				
-				var table = document.createElement("table");
-				table.id = tableId;
-				
-				var html = ``;
-				if (obj === json.monexFee) {
-					html = `<thead><tr><td style="font-size : 15px;" colspan="3">${company}</td></tr></thead><tbody>`;
-						
-				} else {
-					html = `<thead><tr class="company"><td style="font-size : 15px;" colspan="2">${company}</td></tr></thead><tbody>`;
-				}
-				
-				for (var i = 0; i < obj.length; i++) {
-					
-					
-					if (obj === json.monexFee) {
-						html += `\
-							<tr>\
-								<td>${obj[i].maxExcurtionFee}</td>\
-								<td>${obj[i].feeForPc}</td>\
-								<td>${obj[i].feeForPhone}</td>\
-							</tr>
-								`;
-					} else {
-						html += `\
-							<tr>\
-								<td>${obj[i].maxExcurtionFee}</td>\
-								<td>${obj[i].fee}</td>\
-							</tr>
-								`;
-					}
-				}
-
-				table.innerHTML = html + `</tbody>`;
-				
-				var parent = document.getElementById('feeDiv');
-				parent.appendChild(table);
-			}
-			
+		}).then(json => {		
 
 			var matsui = json.matsuiFee;
 			if (matsui !== null) {
-				createTable(matsui, '松井証券', 'matsuiFeeTable');
+				createTable(json, matsui, '松井証券', 'matsuiFeeTable');
 			}
-				
+							
 			var monex = json.monexFee;
 			if (monex !== null) {
-				createTable(monex, 'マネックス証券', 'monexFeeTable');
+				createTable(json, monex, 'マネックス証券', 'monexFeeTable');
 			}
 			
 			var rakuten = json.rakutenFee;
 			if (rakuten !== null) {
-				createTable(rakuten, '楽天証券', 'rakutenFeeTable');
+				createTable(json, rakuten, '楽天証券', 'rakutenFeeTable');
 			}
 			
 			var gmo = json.gmoFee;
 			if (gmo !== null) {
-				createTable(gmo, 'GMOクリック証券', 'gmoFeeTable');
+				createTable(json, gmo, 'GMOクリック証券', 'gmoFeeTable');
 			}
 			
 			var sbi = json.sbiFee;
 			if (sbi !== null) {
-				createTable(sbi, 'SBI証券', 'sbiFeeTable');
-			}
-			
+				createTable(json, sbi, 'SBI証券', 'sbiFeeTable');
+			}			
 		});
-
-	
 }
 
-/*
-`\
-<tr>\
-<td>${matsui[0].maxExcurtionFee}</td>\
-<td>${matsui[0].fee}</td>\
-</tr>\
-<tr>\
-<td>10万円まで</td>\
-<td></td>\
-</tr>\
-<tr>\
-<td>30万円まで</td>\
-<td></td>\
-</tr>\
-<tr>\
-<td>50万円まで</td>\
-<td></td>\
-</tr>\
-<tr>\
-<td>100万円まで</td>\
-<td></td>\
-</tr>\
-<tr>\
-<td>200万円まで</td>\
-<td></td>\
-</tr>\
-<tr>\
-<td>100万円増えるごとに1000円加筆</td>\
-</tr>\
-<tr>\
-<td>1億円越</td>\
-<td></td>\
-</tr>\
-`;
-*/
+function showTable(id) {
+	var url = "/fee"
+	fetch(url)
+		.then(response => {
+			return  response.json();
+			
+		}).then(json => {	
+			
+			switch(id) {
+		
+			case 'matsui':
+				var matsui = json.matsuiFee;
+				createTable(json, matsui, '松井証券', 'matsuiFeeTable');
+				break;
+				
+			case 'monex':
+				var monex = json.monexFee;
+				createTable(json, monex, 'マネックス証券', 'monexFeeTable');
+				break;
+			
+			case 'rakuten':
+				var rakuten = json.rakutenFee;
+				createTable(json, rakuten, '楽天証券', 'rakutenFeeTable');
+				break;
+			
+			case 'gmo':
+				var gmo = json.gmoFee;
+				createTable(json, gmo, 'GMOクリック証券', 'gmoFeeTable');
+				break;
+			
+			case 'sbi':
+				var sbi = json.sbiFee;
+				createTable(json, sbi, 'SBI証券', 'sbiFeeTable');
+				break;
+			}
+			
+			
+		});
+}
+
+function closeTable(name) {
+	
+	var parent = document.getElementById('feeDiv');
+	var table = document.getElementById(name);
+	parent.removeChild(table);
+}
+
+function createTable(json, obj, company, tableId) {
+	
+	var id = document.getElementById(tableId);
+	if (id !== null) { return; }
+	
+	var table = document.createElement("table");
+	table.id = tableId;
+	
+	var html = ``;
+	if (obj === json.monexFee) {
+		html = `<thead><tr><td style="font-size : 15px;" colspan="3">${company}</td></tr></thead><tbody>`;
+			
+	} else {
+		html = `<thead><tr class="company"><td style="font-size : 15px;" colspan="2">${company}</td></tr></thead><tbody>`;
+	}
+	
+	for (var i = 0; i < obj.length; i++) {
+		
+		if (obj === json.monexFee) {
+			html += `\
+				<tr>\
+					<td>${obj[i].maxExcurtionFee}</td>\
+					<td>${obj[i].feeForPc}</td>\
+					<td>${obj[i].feeForPhone}</td>\
+				</tr>
+					`;
+		} else {
+			html += `\
+				<tr>\
+					<td>${obj[i].maxExcurtionFee}</td>\
+					<td>${obj[i].fee}</td>\
+				</tr>
+					`;
+		}
+	}
+	//last row
+	if (obj === json.monexFee) {
+		html += `<tr><td colspan="3"><a name=${tableId} href="javascript:void(0);" onclick="closeTable(name);">close</a></td></tr></tbody>`;
+			
+	} else {
+		html += `<tr><td colspan="2"><a name=${tableId} href="javascript:void(0);" onclick="closeTable(name);">close</a></td></tr></tbody>`;
+	}
+
+	table.innerHTML = html;
+	
+	var parent = document.getElementById('feeDiv');
+	parent.appendChild(table);
+}
 
 
